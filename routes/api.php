9a1937247b23a61b02032e8025ed6e272fb64714
Fabian Espinosa <fabianespinosa1988@gmail.com>
2023-03-18 03:06:54 +0100

    feat: Movies CRUD api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\MovieController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,9 +18,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/movie/create', [MovieController::class, 'store']);
+    Route::post('/movie/update/{id}', [MovieController::class, 'update']);
+    Route::get('/movie/{id}', [MovieController::class, 'show']);
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
+    Route::delete('/movie/{id}', [MovieController::class, 'destroy']);
 });
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/movies', [MovieController::class, 'index']);
